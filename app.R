5b070540e9e2f3444efacc413e82755207d1498d
Tony Boyles <Anthony@Boyles.cc>
2016-07-29 16:41:38 -0400

    Fixed the App. It's now a single-file Shiny app.

---
+++ app.R
@@ -0,0 +1,77 @@
+#!/usr/bin/env R
+
+source("deps.R")
+
+load("data/Medicare_Data.rdata")
+
+shinyApp(
+  navbarPage("Health Cost Explorer", theme = shinytheme("cerulean"),
+    tabPanel("Data",
+      fluidRow(
+        column(2, offset = 1, id = "controls",
+        	selectInput("year",  "Year", 2011:2014, selected = 2014),
+          selectInput("state", "State", as.character(StateCentroids$Code), selected = "VA"),
+          selectInput("code",  "Procedure", list("Outpatient" = OutpatientCodes$procedure, "Inpatient" = InpatientCodes$Procedure))),
+        column(8,
+        	tabsetPanel(
+        		tabPanel("Map", leafletOutput("mymap")),
+        		tabPanel("Table", dataTableOutput("mytable"))
+        	)
+        )
+      )
+    ),
+    tabPanel("About", fluidRow(column(6, offset = 3,
+      includeMarkdown("Readme.md"),
+      tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "main.css"))
+    )))
+  ),
+
+  shinyServer(function(input, output, session) {
+
+   	thisData <- reactive({
+  		if(input$code %in% InpatientCodes$Procedure){
+  			code <- InpatientCodes %>%
+  				filter(Procedure==input$code)
+        InpatientData %>%
+        	filter(year==input$year, DRG==code$DRG) ->
+  				data
+  		} else {
+  			code <- OutpatientCodes %>%
+  				filter(procedure==input$code)
+  			OutpatientData %>%
+        	filter(year==input$year, code==code$code) ->
+  				data
+  		}
+  		Providers %>%
+        filter(`Provider State`==input$state) %>%
+  			inner_join(data)
+  	})
+  	
+    output$mymap <- renderLeaflet({
+      state <- StateCentroids %>% filter(Code==input$state)
+      data <- thisData()
+      if(nrow(data)>0){
+        leaflet(data) %>%
+          setView(lng = state$Longitude[1], lat = state$Latitude[1], zoom = 8) %>%
+          addProviderTiles("CartoDB.Positron") %>%
+          addCircleMarkers(
+            lat = ~latitude,
+            lng = ~longitude,
+            popup = ~paste0(
+              "<b>",str_to_title(`Provider Name`),"</b><br />",
+              "Average Total Cost: $", format(round(`Average Total Payments`,2), decimal.mark=".", big.mark=","), "<br />",
+              "Treatments Performed: ", performed))
+      } else {
+        leaflet() %>%
+          setView(lng = state$Longitude[1], lat = state$Latitude[1], zoom = 8) %>%
+          addProviderTiles("CartoDB.Positron")
+      }
+    })
+    output$mytable <- renderDataTable({
+			thisData() %>%
+        mutate(Location = str_to_title(`Provider City`)) %>%
+        select(`Provider Name`, Location, `Treatments Performed`=performed, `Average Total Payments`) %>%
+        datatable(options=list(paging=FALSE, searching=FALSE, responsive=TRUE), rownames = FALSE, escape = FALSE)
+    })
+  })
+)
--- global.R
+++
@@ -1,7 +0,0 @@
-library(shiny)
-library(DT)
-library(leaflet)
-library(readr)
-
-Procedures <- read_csv("data/Procedures2.csv", col_types = "cc")
-StateCentroids <- read_csv("data/StateCentroids.csv")
